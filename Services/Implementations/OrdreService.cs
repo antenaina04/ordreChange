e58c50ddd8dc4978b1d8d39442789758e3de0203
Antenaina Randrianantoandro <57990364+antenaina04@users.noreply.github.com>
2024-11-12 21:45:18 +0300

    🔨 Enregistrement historique de creation d'Ordre

--- Services/Implementations/OrdreService.cs
+++ Services/Implementations/OrdreService.cs
@@ -29,15 +29,26 @@ namespace ordreChange.Services.Implementations
                 Statut = "En attente",
                 TypeTransaction = typeTransaction,
                 DateCreation = DateTime.UtcNow,
-                //IdAgent = agentId
                 Agent = agent
             };
 
             await _unitOfWork.Ordres.AddAsync(ordre);
+
+            var historique = new HistoriqueOrdre
+            {
+                Date = DateTime.UtcNow,
+                Statut = ordre.Statut,
+                Montant = ordre.Montant,
+                Ordre = ordre
+            };
+            await _unitOfWork.HistoriqueOrdres.AddAsync(historique);
+
+            // Appliquer les changements
             await _unitOfWork.CompleteAsync();
 
             return ordre;
         }
+
         public async Task<Ordre?> GetOrdreByIdAsync(int id)
         {
             return await _unitOfWork.Ordres.GetByIdAsync(id);
@@ -45,22 +56,18 @@ namespace ordreChange.Services.Implementations
 
         public async Task<bool> ValiderOrdreAsync(int ordreId, int agentId)
         {
-            // Vérifiez que l'agent est un Validateur
             var agent = await _unitOfWork.Agents.GetByIdAsync(agentId);
             if (agent == null || agent.Role != Role.Validateur)
                 throw new InvalidOperationException("L'agent n'est pas autorisé à valider des ordres.");
 
-            // Récupérez l'ordre
             var ordre = await _unitOfWork.Ordres.GetByIdAsync(ordreId);
             if (ordre == null || ordre.Statut != "En attente")
                 return false;
 
-            // Mettre à jour le statut de l'ordre
             ordre.Statut = "Validé";
             ordre.DateDerniereModification = DateTime.UtcNow;
             _unitOfWork.Ordres.Update(ordre);
 
-            // Ajouter une entrée dans l'historique
             var historique = new HistoriqueOrdre
             {
                 Date = DateTime.UtcNow,
